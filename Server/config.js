e06de38455ae409305b647c6ff962843f7458f78
rajeswari-subramanian <rajilechrame@gmail.com>
2020-11-28 14:31:54 +0530

    gitignore

--- Server/config.js
+++ Server/config.js
@@ -1,5 +1,9 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 module.exports = {
-    serviceID: "VAa6360d0f828365e245e9fc2400cd162f",
-    accountSID: "AC6335647114b1ba88b83f3163866eb775",
-    authToken: "3c17058dd814b7ddb8ee9502a342847d",
+    serviceID: `${process.env.serviceID}`,
+    accountSID: `${process.env.accountSID}`,
+    authToken: `${process.env.authToken}`,
 };
+
--- Server/server.js
+++ Server/server.js
@@ -3,30 +3,25 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const app = express();
+dotenv.config();
 
 //RAZORPAY
-
 const path = require("path");
 const shortid = require("shortid");
 const Razorpay = require("razorpay");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 const razorpay = new Razorpay({
-    key_id: "rzp_test_8sYueDGWseWPkq",
-    key_secret: "t9I6Fb4f6EveXqYsr4JQqqN8",
+    key_id: `${process.env.key_id}`,
+    key_secret: `${process.env.key_secret}`,
 });
 app.post("/verification", (req, res) => {
-    // do a validation
     const secret = "12345678";
-
     const crypto = require("crypto");
-
     const shasum = crypto.createHmac("sha256", secret);
     shasum.update(JSON.stringify(req.body));
     const digest = shasum.digest("hex");
-
     //console.log(digest, req.headers['x-razorpay-signature'])
-
     if (digest === req.headers["x-razorpay-signature"]) {
         //console.log('request is legit')
         // process it
@@ -44,14 +39,12 @@ app.post("/razorpay", async (req, res) => {
     const payment_capture = 1;
     const amount = req.body.amount;
     const currency = "INR";
-
     const options = {
         amount: amount * 100,
         currency,
         receipt: shortid.generate(),
         payment_capture,
     };
-
     try {
         const response = await razorpay.orders.create(options);
         //console.log("rrrresponae", response);
@@ -64,16 +57,9 @@ app.post("/razorpay", async (req, res) => {
         console.log(error);
     }
 });
-
 //RAZORPAY
 
-let donego = require("./donegoitems.json");
-let users = require("./userDet.json");
-
-dotenv.config();
 const donegoRoute = require("./routes/DonegoRoutes");
-const donegoModel = require("./models/DonegoModel");
-const userModel = require("./models/userDetailModel");
 
 app.use(cors());
 app.use(express.json());
@@ -82,35 +68,8 @@ mongoose.connect(
     process.env.ATLAS_URI,
     { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
     (err) => {
-        if (err) throw err;
-        if (
-            donegoModel.collection.countDocuments(function (err, count) {
-                if (!err && count === 0) {
-                    donegoModel
-                        .insertMany(donego)
-                        .then(() => {
-                            console.log("Data inserted");
-                        })
-                        .catch((error) => {
-                            console.log(error);
-                        });
-                }
-            })
-        );
-        if (
-            userModel.collection.countDocuments(function (err, count) {
-                if (!err && count === 0) {
-                    userModel
-                        .insertMany(users)
-                        .then(() => {
-                            console.log("Data inserted");
-                        })
-                        .catch((error) => {
-                            console.log(error);
-                        });
-                }
-            })
-        );
+        if (err) console.log("The database is not connected");
+        else console.log("The database is connected");
     }
 );
 
